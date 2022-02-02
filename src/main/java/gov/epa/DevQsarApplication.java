b7cb1d6d11ac2c3114847fc3eb2e53a2263ead22
gsincl01_VDI <Sinclair.Gabriel@epa.gov>
2022-02-02 10:44:55 -0500

    Fixed port collision

--- src/main/java/gov/epa/DevQsarApplication.java
+++ src/main/java/gov/epa/DevQsarApplication.java
@@ -2,12 +2,27 @@ package gov.epa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class DevQsarApplication {
 	
+	@Component
+	public class ServerPortCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+	    @Override
+	    public void customize(ConfigurableWebServerFactory factory) {
+	        factory.setPort(9090);
+	    }
+	}
+	
 	public static void main(String[] args) {
-		SpringApplication.run(DevQsarApplication.class, args);
+		try {
+	        SpringApplication.run(DevQsarApplication.class, args);
+	    } catch (Exception e) {
+	        e.printStackTrace(); 
+	    }
 	}
 
 }
