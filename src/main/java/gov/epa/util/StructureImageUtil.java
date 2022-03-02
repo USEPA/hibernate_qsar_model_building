790779d1ede8a60b873cf77365a6f3e0509282d7
AA\TMARTI02 <martin.todd@epa.gov>
2022-03-02 08:58:23 -0500

    Adding comments

--- src/main/java/gov/epa/util/StructureImageUtil.java
+++ src/main/java/gov/epa/util/StructureImageUtil.java
@@ -14,21 +14,51 @@ import org.openscience.cdk.smiles.SmilesParser;
 import com.epam.indigo.IndigoException;
 
 
-
+/**
+ * Class to create image files and embedded image link in html
+ * 
+ * @author TMARTI02
+ *
+ */
 public class StructureImageUtil {
 	
 	private static final SmilesParser parser = new SmilesParser(DefaultChemObjectBuilder.getInstance());
 
-	
+	/**
+	 * Write image to file
+	 * 
+	 * @param ac
+	 * @param filepath
+	 * @throws IOException
+	 * @throws CDKException
+	 */
 	public static void writeImageFile(AtomContainer ac, String filepath) throws IOException, CDKException {
 		new DepictionGenerator().withAtomColors().withZoom(1.5).depict(ac).writeTo(filepath);
 	}
 	
+	/**
+	 * Write image to byte array
+	 * 
+	 * @param ac
+	 * @return
+	 * @throws IOException
+	 * @throws CDKException
+	 */
 	public static byte[] writeImageBytes(AtomContainer ac) throws IOException, CDKException {
 		ByteArrayOutputStream baos=new ByteArrayOutputStream();
 		new DepictionGenerator().withAtomColors().withZoom(1.5).depict(ac).writeTo(Depiction.PNG_FMT,baos);
 		return baos.toByteArray();
 	}
+	
+	/**
+	 * Creates image url so that image can be embedded inside html page using base64 encoded image
+	 * 
+	 * @param smiles
+	 * @return
+	 * @throws IOException
+	 * @throws CDKException
+	 * @throws IndigoException
+	 */
 	public static String generateImgSrc(String smiles) throws IOException, CDKException, IndigoException {
 //		String inchikey = StructureUtil.indigoInchikeyFromSmiles(smiles);
 		
