0627f1467ae47a78969d1811abce1c0378f8fbe2
AA\TMARTI02 <martin.todd@epa.gov>
2022-06-15 12:30:38 -0400

    Added some smiles methods to datasetfilewriter

--- src/main/java/gov/epa/run_from_java/scripts/DatasetFileWriter.java
+++ src/main/java/gov/epa/run_from_java/scripts/DatasetFileWriter.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import gov.epa.databases.dev_qsar.qsar_datasets.entity.DataPoint;
 import gov.epa.databases.dev_qsar.qsar_datasets.entity.Dataset;
@@ -12,7 +14,11 @@ import gov.epa.databases.dev_qsar.qsar_datasets.service.DataPointService;
 import gov.epa.databases.dev_qsar.qsar_datasets.service.DataPointServiceImpl;
 import gov.epa.databases.dev_qsar.qsar_datasets.service.DatasetService;
 import gov.epa.databases.dev_qsar.qsar_datasets.service.DatasetServiceImpl;
+import gov.epa.databases.dev_qsar.qsar_descriptors.entity.Compound;
+import gov.epa.databases.dev_qsar.qsar_descriptors.entity.DescriptorSet;
 import gov.epa.databases.dev_qsar.qsar_descriptors.entity.DescriptorValues;
+import gov.epa.databases.dev_qsar.qsar_descriptors.service.CompoundService;
+import gov.epa.databases.dev_qsar.qsar_descriptors.service.CompoundServiceImpl;
 import gov.epa.databases.dev_qsar.qsar_descriptors.service.DescriptorValuesService;
 import gov.epa.databases.dev_qsar.qsar_descriptors.service.DescriptorValuesServiceImpl;
 import gov.epa.endpoints.models.ModelData;
@@ -51,12 +57,70 @@ public class DatasetFileWriter {
 		return writeWithoutSplitting(dataset.getName(), descriptorSetName, outputFolderPath, fetchDtxcids);
 	}
 	
+	public void writeQSARSmilesCID(Long datasetId, String outputFolderPath) {
+		Dataset dataset = datasetService.findById(datasetId);
+		
+		if (dataset==null) {
+			return;
+		}
+		
+		List<DataPoint> dataPoints = dataPointService.findByDatasetName(dataset.getName());
+		
+		String outputFileName = dataset.getName()+ "_SmilesDTXCID.tsv";
+		String outputFilePath = outputFolderPath + (outputFolderPath.endsWith("/") ? "" : "/") + outputFileName;
+		File outputFile = new File(outputFilePath);
+		outputFile.getParentFile().mkdirs();
+		
+		String instances = generateSmilesDTXCID(dataPoints);
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFilePath))) {
+			bw.write(instances);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	public static String generateSmilesDTXCID(List<DataPoint> dataPoints) {
+		Map<String, DataPoint> dataPointsMap = dataPoints.stream().collect(Collectors.toMap(dp -> dp.getCanonQsarSmiles(), dp -> dp));
+		
+		CompoundService compoundService = new CompoundServiceImpl();
+		
+		String instanceHeader="QSARSmiles\tDTXCID\tPropertyValue\r\n";
+		StringBuilder sbOverall = new StringBuilder(instanceHeader);
+		
+		for (String smiles:dataPointsMap.keySet()) {
+			DataPoint dp = dataPointsMap.get(smiles);
+			
+			if (dp!=null && !dp.getOutlier()) {
+				
+				List<Compound> compounds = compoundService.findByCanonQsarSmiles(smiles);
+				if (compounds!=null) {
+					
+					for (Compound compound:compounds) {
+						String instance = smiles+"\t"+compound.getDtxcid()+"\t"+dp.getQsarPropertyValue()+"\r\n";
+						sbOverall.append(instance);
+					}
+				}
+				
+			}
+		}
+		
+		return sbOverall.toString();
+	}
+
+	
 	public static void main(String[] args) {
 		DatasetFileWriter writer = new DatasetFileWriter();
 //		writer.writeWithoutSplitting(38L, "T.E.S.T. 5.1", "data/dev_qsar/dataset_files/");
 //		writer.writeWithoutSplitting(36L, "T.E.S.T. 5.1", "data/dev_qsar/dataset_files/");
 //		writer.writeWithoutSplitting(42L, "T.E.S.T. 5.1", "data/dev_qsar/dataset_files/");
-		writer.writeWithoutSplitting(43L, "T.E.S.T. 5.1", "data/dev_qsar/dataset_files/", true);
+//		writer.writeWithoutSplitting(31L, "T.E.S.T. 5.1", "data/dev_qsar/dataset_files/", true);
+//		writer.writeWithoutSplitting(34L, "T.E.S.T. 5.1", "data/dev_qsar/dataset_files/", true);
+		writer.writeQSARSmilesCID(31L,"data/dev_qsar/dataset_files/");
+//		writer.writeQSARSmilesCID(34L,"data/dev_qsar/dataset_files/");
 	}
 
 }
