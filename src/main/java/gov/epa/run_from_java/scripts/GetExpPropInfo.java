8e21a05ddd0b85bbd9589a74b65c3f1b9385f9b3
AA\TMARTI02 <martin.todd@epa.gov>
2022-06-23 10:20:32 -0400

    Added more information to the PFAS json file

--- src/main/java/gov/epa/run_from_java/scripts/GetExpPropInfo.java
+++ src/main/java/gov/epa/run_from_java/scripts/GetExpPropInfo.java
@@ -19,7 +19,7 @@ import gov.epa.util.wekalite.Instances;
 
 public class GetExpPropInfo {
 	
-	static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	static Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 
 	static String createQuery1(String smiles, int fk_dataset_id) {
 		String sql="select dp.canon_qsar_smiles,dp.qsar_property_value, dpc.exp_prop_id\n"; 
@@ -39,6 +39,34 @@ public class GetExpPropInfo {
 	static String lookupUnits(int id, Connection conn) {
 		String sql="select \"name\" from exp_prop.units u \r\n"
 				+ "where u.id="+id;
+		return runSQL(conn, sql);
+	}
+	
+	static String lookupDTXCID(String smiles, Connection conn) {
+		String sql="select dtxcid from qsar_descriptors.compounds t \r\n"
+				+ "where t.canon_qsar_smiles='"+smiles+"';";
+		return runSQL(conn, sql);
+	}
+
+	static String lookupQSAR_units(int id, Connection conn) {
+		//TODO change to inner join instead of 2 queries
+		
+		String sql="select fk_unit_id from qsar_datasets.datasets t \r\n"
+				+ "where t.id='"+id+"';";
+		String fk_units_id=runSQL(conn, sql);
+
+		
+		sql="select name from qsar_datasets.units t \r\n"
+				+ "where t.id='"+fk_units_id+"';";
+		String units=runSQL(conn, sql);
+
+//		System.out.println(units);
+		return units;
+		
+	}
+
+	
+	private static String runSQL(Connection conn, String sql) {
 		try {
 			Statement st = conn.createStatement();			
 			ResultSet rs = st.executeQuery(sql);
@@ -50,6 +78,16 @@ public class GetExpPropInfo {
 			e.printStackTrace();
 		}
 		return "";
+	}
+	
+	
+	static String lookupMolWt(String dtxcid, Connection connDSSTOX) {
+		String sql="select mol_weight from compounds t \r\n"
+				+ "where t.dsstox_compound_id='"+dtxcid+"';";
+		
+//		System.out.println(sql);
+		
+		return runSQL(connDSSTOX, sql);
 	}
 	
 	static String getAllValues(int id, String table, Connection conn,JsonObject jo) {
@@ -96,8 +134,26 @@ public class GetExpPropInfo {
 		}
 	}
 	
+	static Connection getConnectionDSSTOX() {
+		String host = System.getenv().get("DSSTOX_HOST");
+		String port = System.getenv().get("DSSTOX_PORT");
+		String db = System.getenv().get("DSSTOX_DATABASE");
+
+		String url = "jdbc:mysql://" + host + ":" + port + "/" + db;
+		String user = System.getenv().get("DSSTOX_USER");
+		String password = System.getenv().get("DSSTOX_PASS");
+
+		try {
+			Connection conn = DriverManager.getConnection(url, user, password);
+			return conn;
+		} catch (Exception ex) {
+			return null;
+		}
+	}
+
 	
-	static void getDataForSmiles(Connection conn, String smiles,int id_dataset, JsonArray ja) {
+	
+	static void getDataForSmiles(Connection conn, Connection connDSSTOX, String smiles,int id_dataset, JsonArray ja) {
 
 		try {
 			
@@ -106,60 +162,75 @@ public class GetExpPropInfo {
 
 			ResultSet rs = st.executeQuery(sql);
 			
+			JsonObject jo=null;
+			JsonArray jaRecords=null;
+						
 			
 			while (rs.next()) {
 
-				JsonObject jo=new JsonObject();
+				if (jo==null) {
+					jo=new JsonObject();
+					ja.add(jo);
+					
+					jo.addProperty("canon_qsar_smiles",rs.getString(1));
+					jo.addProperty("qsar_property_value",rs.getString(2));
 
-				jo.addProperty("canon_qsar_smiles",rs.getString(1));
-				jo.addProperty("qsar_property_value",rs.getString(2));
+					String qsar_units=lookupQSAR_units(id_dataset, conn);
+					jo.addProperty("qsar_property_units", qsar_units);
+
+					String dtxcid=lookupDTXCID(rs.getString(1), conn);
+					jo.addProperty("dtxcid",dtxcid);
+					
+					String mol_weight=lookupMolWt(dtxcid, connDSSTOX);
+					jo.addProperty("mol_weight",mol_weight);
+
+					jaRecords=new JsonArray();
+					jo.add("Records", jaRecords);
+				}
+				
+				
+				JsonObject joRecord=new JsonObject();
 				
 				String str_exp_prop_id = rs.getString(3);
 				
 				Integer exp_prop_id=Integer.parseInt(str_exp_prop_id.replace("EXP", ""));
-				jo.addProperty("exp_prop_id",exp_prop_id);
+				joRecord.addProperty("exp_prop_id",exp_prop_id);
+				getAllValues(exp_prop_id, "exp_prop.property_values",conn, joRecord);
 
-				getAllValues(exp_prop_id, "exp_prop.property_values",conn, jo);
 				
-				jo.addProperty("qsar_property_units", "-log10(M)");//TODO look up from id_dataset instead of hardcode
-				
-				
-				jo.addProperty("units",lookupUnits(jo.get("fk_unit_id").getAsInt(), conn));
+				joRecord.addProperty("units",lookupUnits(joRecord.get("fk_unit_id").getAsInt(), conn));
 
 
-				if (!jo.get("fk_literature_source_id").isJsonNull()) {
-					int id=jo.get("fk_literature_source_id").getAsInt();
-					getAllValues(id,"exp_prop.literature_sources", conn, jo);
+				if (!joRecord.get("fk_literature_source_id").isJsonNull()) {
+					int id=joRecord.get("fk_literature_source_id").getAsInt();
+					getAllValues(id,"exp_prop.literature_sources", conn, joRecord);
 				}
 					
-				if (!jo.get("fk_public_source_id").isJsonNull()) {
-					int id=jo.get("fk_public_source_id").getAsInt();
-					getAllValues(id,"exp_prop.public_sources", conn, jo);
+				if (!joRecord.get("fk_public_source_id").isJsonNull()) {
+					int id=joRecord.get("fk_public_source_id").getAsInt();
+					getAllValues(id,"exp_prop.public_sources", conn, joRecord);
 				}
 					
-				int id=jo.get("fk_source_chemical_id").getAsInt();
-				getAllValues(id,"exp_prop.source_chemicals", conn, jo);
+				int id=joRecord.get("fk_source_chemical_id").getAsInt();
+				getAllValues(id,"exp_prop.source_chemicals", conn, joRecord);
 
-				jo.remove("access_date");
-				jo.remove("created_at");
-				jo.remove("updated_at");
-				jo.remove("created_by");
-				jo.remove("keep");
-				
-				jo.remove("id");
-				jo.remove("exp_prop_id");				
-				jo.remove("fk_unit_id");				
-				jo.remove("fk_property_id");
-				jo.remove("fk_source_chemical_id");
-				jo.remove("fk_public_source_id");
-				jo.remove("fk_literature_source_id");
-				
-				ja.add(jo);     			
-//				rs2.close();
+				joRecord.remove("access_date");
+				joRecord.remove("created_at");
+				joRecord.remove("updated_at");
+				joRecord.remove("created_by");
+				joRecord.remove("keep");
+//				
+				joRecord.remove("id");
+				joRecord.remove("fk_unit_id");				
+				joRecord.remove("fk_property_id");
 
+//				jo.remove("exp_prop_id");				
+//				jo.remove("fk_source_chemical_id");
+//				jo.remove("fk_public_source_id");
+//				jo.remove("fk_literature_source_id");
+				
+				jaRecords.add(joRecord);     			
 			}
-			
-					
 			rs.close();
 
 
@@ -171,8 +242,8 @@ public class GetExpPropInfo {
 	public static void main(String[] args) {
 		
 		Connection conn=getConnection();
+		Connection connDSSTOX=getConnectionDSSTOX();
 		String folder="data\\dev_qsar\\dataset_files\\";
-		
 		
 //		String inputFileName="Standard Water solubility from exp_prop_T.E.S.T. 5.1_T=PFAS only, P=PFAS_prediction.tsv";
 //		String outputFileName="Water solubility PFAS prediction records.json";
@@ -181,10 +252,10 @@ public class GetExpPropInfo {
 		String outputFileName="Water solubility PFAS training records.json";
 		
 		
-		getRecords(conn, folder, inputFileName,outputFileName);
+		getRecords(conn, connDSSTOX, folder, inputFileName,outputFileName);
 	}
 
-	private static void getRecords(Connection conn, String folder, String inputFileName,String outputFileName) {
+	private static void getRecords(Connection conn, Connection connDSSTOX, String folder, String inputFileName,String outputFileName) {
 		CSVLoader c=new CSVLoader();
 		try {
 			Instances instances=c.getDataSetFromFile(folder+inputFileName,"\t");
@@ -193,8 +264,9 @@ public class GetExpPropInfo {
 			int id_dataset=31;
 			
 			for (int i=0;i<instances.numInstances();i++) {
+				System.out.println(i);
 				String smiles=instances.instance(i).getName();
-				getDataForSmiles(conn, smiles,id_dataset,ja);
+				getDataForSmiles(conn, connDSSTOX, smiles,id_dataset,ja);
 				
 			}
 			
@@ -203,7 +275,7 @@ public class GetExpPropInfo {
 			fw.flush();
 			fw.close();
 			
-			System.out.println(ja.size());
+//			System.out.println(ja.size());
 			
 			
 		} catch (IOException e) {
