9a4323e5d582e0ecd85923a658906814c71d7ecb
AA\CRAMSLAN <ramsland.christian@epa.gov>
2022-11-21 14:01:19 -0500

    new scripts added to datasetcreator script

--- src/main/java/gov/epa/endpoints/datasets/DatasetCreatorScript.java
+++ src/main/java/gov/epa/endpoints/datasets/DatasetCreatorScript.java
@@ -27,7 +27,8 @@ public class DatasetCreatorScript {
 	public static void main(String[] args) {
 		//createLogP();
 		//createHLC_Todd();		
-		createWS_tmm();
+		// createWS_tmm();
+		createMP();
 		
 //		DatasetServiceImpl ds=new DatasetServiceImpl();
 //		ds.delete(77);
@@ -131,7 +132,6 @@ public class DatasetCreatorScript {
 
 	}
 
-
 	// methods like these 
 	public static void createHLC() {
 		// comment for diff
@@ -202,8 +202,10 @@ public class DatasetCreatorScript {
 
 	}
 
+	/**
+	 * create melting point dataset based on CAS registration
+	 */
 	public static void createMPRegisteredCAS() {
-		// comment for diff
 		SciDataExpertsStandardizer sciDataExpertsStandardizer = new SciDataExpertsStandardizer(DevQsarConstants.QSAR_READY);
 		DatasetCreator creator = new DatasetCreator(sciDataExpertsStandardizer, "cramslan");
 
@@ -213,13 +215,11 @@ public class DatasetCreatorScript {
 		List<BoundParameterValue> bounds = new ArrayList<BoundParameterValue>();
 		bounds.add(PressureBound);
 
-
-
 		MappingParams casrnMappingParams = new MappingParams(DevQsarConstants.MAPPING_BY_CASRN, null,
 				true, false, false, false, false, false, true, null,true);
 
 		String casrnMappingName = "ExpProp_MP_CASRN";
-		String casrnMappingDescription = "Exprop HLC with 740 < P (mmHg) < 780";
+		String casrnMappingDescription = "Exprop MP with 740 < P (mmHg) < 780";
 		DatasetParams casrnMappedParams = new DatasetParams(casrnMappingName, 
 				casrnMappingDescription, 
 				propertyName,
@@ -228,9 +228,53 @@ public class DatasetCreatorScript {
 
 		creator.createPropertyDataset(casrnMappedParams, false);
 	}
+	
+	/**
+	 * create melting point dataset from expprop records based on list registration
+	 */
+	public static void createMP() {
+		
+		SciDataExpertsStandardizer sciDataExpertsStandardizer = new SciDataExpertsStandardizer(DevQsarConstants.QSAR_READY);
+		DatasetCreator creator = new DatasetCreator(sciDataExpertsStandardizer, "cramslan");
+		
+		ArrayList<String> listNameArray = new ArrayList<String>();
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_1_to_40000");
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_40001_to_80000");
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_80001_to_120000");
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_120001_to_160000");
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_160001_to_200000");
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_200001_to_240000");
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_240001_to_280000");
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_280001_to_320000");
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_320001_to_360000");
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_360001_to_400000");
+		listNameArray.add("ExpProp_MP_WithChemProp_063022_400001_to_427100");
 
+		String propertyName = DevQsarConstants.MELTING_POINT;
+
+		BoundParameterValue PressureBound = new BoundParameterValue("Pressure", 740.0, 780.0, true);
+		List<BoundParameterValue> bounds = new ArrayList<BoundParameterValue>();
+		bounds.add(PressureBound);
+		
+		MappingParams listMappingParams = new MappingParams(DevQsarConstants.MAPPING_BY_LIST, null, isNaive,
+				useValidation, requireValidation, resolveConflicts, validateConflictsTogether,
+				omitOpsinAmbiguousNames, omitUvcbNames, listNameArray, omitSalts);
+
+		String listMappingName = "Standard Melting Point from exp_prop";
+		String listMappingDescription = "Melting Point 740 < P (mmHg) < 780";
+		DatasetParams listMappedParams = new DatasetParams(listMappingName, 
+				listMappingDescription, 
+				propertyName,
+				listMappingParams,
+				bounds);
+		creator.createPropertyDataset(listMappedParams, false);
+
+	}
+
+	/**
+	 * creates Boiling point dataset using records from chemreg with CAS registration
+	 */
 	public static void createBPRegisteredCAS() {
-		// comment for diff
 		SciDataExpertsStandardizer sciDataExpertsStandardizer = new SciDataExpertsStandardizer(DevQsarConstants.QSAR_READY);
 		DatasetCreator creator = new DatasetCreator(sciDataExpertsStandardizer, "cramslan");
 
@@ -239,8 +283,6 @@ public class DatasetCreatorScript {
 		BoundParameterValue PressureBound = new BoundParameterValue("Pressure", 740.0, 780.0, true);
 		List<BoundParameterValue> bounds = new ArrayList<BoundParameterValue>();
 		bounds.add(PressureBound);
-
-
 
 		MappingParams casrnMappingParams = new MappingParams(DevQsarConstants.MAPPING_BY_CASRN, null,
 				true, false, false, false, false, false, true, null,true);
@@ -288,6 +330,9 @@ public class DatasetCreatorScript {
 
 
 
+	/**
+	 * creates Henry's law constant dataset using records from chemreg with CAS registration.
+	 */
 	public static void createHLCRegisteredCAS() {
 		// comment for diff
 		SciDataExpertsStandardizer sciDataExpertsStandardizer = new SciDataExpertsStandardizer(DevQsarConstants.QSAR_READY);
@@ -340,22 +385,48 @@ public class DatasetCreatorScript {
 
 	}
 
+	
+	/**
+	 * create boiling point dataset based on chemreg lists
+	 */
 	public static void createBP() {
+		
+		ArrayList<String> listNameArray = new ArrayList<String>();
+		listNameArray.add("ExpProp_BP_072522_Import_1_to_20000");
+		listNameArray.add("ExpProp_BP_072522_Import_20001_to_40000");
+		listNameArray.add("ExpProp_BP_072522_Import_40001_to_60000");
+		listNameArray.add("ExpProp_BP_072522_Import_60001_to_80000");
+		listNameArray.add("ExpProp_BP_072522_Import_80001_to_100000");
+		listNameArray.add("ExpProp_BP_072522_Import_100001_to_120000");
+		listNameArray.add("ExpProp_BP_072522_Import_140001_to_160000");
+		listNameArray.add("ExpProp_BP_072522_Import_160001_to_180000");
+		listNameArray.add("ExpProp_BP_072522_Import_180001_to_200000");
+		listNameArray.add("ExpProp_BP_072522_Import_200001_to_220000");
+		listNameArray.add("ExpProp_BP_072522_Import_240001_to_260000");
+		listNameArray.add("ExpProp_BP_072522_Import_260001_to_280000");
+		listNameArray.add("ExpProp_BP_072522_Import_280001_to_300000");
+		listNameArray.add("ExpProp_BP_072522_Import_300001_to_320000");
+		listNameArray.add("ExpProp_BP_072522_Import_340001_to_360000");
+		listNameArray.add("ExpProp_BP_072522_Import_360001_to_363160");
+
+		
 		SciDataExpertsStandardizer sciDataExpertsStandardizer = new SciDataExpertsStandardizer(DevQsarConstants.QSAR_READY);
 		DatasetCreator creator = new DatasetCreator(sciDataExpertsStandardizer, "cramslan");
 
 		String propertyName = DevQsarConstants.BOILING_POINT;
-		String listName = "ExpProp_BP_072522";
 
 		BoundParameterValue PressureBound = new BoundParameterValue("Pressure", 740.0, 780.0, true);
 		List<BoundParameterValue> bounds = new ArrayList<BoundParameterValue>();
 		bounds.add(PressureBound);
+		
+		MappingParams listMappingParams = new MappingParams(DevQsarConstants.MAPPING_BY_LIST, null, isNaive,
+				useValidation, requireValidation, resolveConflicts, validateConflictsTogether,
+				omitOpsinAmbiguousNames, omitUvcbNames, listNameArray, omitSalts);
 
 
-		MappingParams listMappingParams = new MappingParams(DevQsarConstants.MAPPING_BY_LIST, listName, 
-				false, true, false, true, true, false, false, null,true);
+
 		String listMappingName = "Standard Boiling Point from exp_prop";
-		String listMappingDescription = "Melting Point 740 < P (mmHg) < 780";
+		String listMappingDescription = "Boiling Point 740 < P (mmHg) < 780";
 		DatasetParams listMappedParams = new DatasetParams(listMappingName, 
 				listMappingDescription, 
 				propertyName,
@@ -363,8 +434,8 @@ public class DatasetCreatorScript {
 				bounds);
 		creator.createPropertyDataset(listMappedParams, false);
 
-
 	}
+
 
 	public static void createbcf2() {
 		System.out.println("eclipse recognizes new code2");
