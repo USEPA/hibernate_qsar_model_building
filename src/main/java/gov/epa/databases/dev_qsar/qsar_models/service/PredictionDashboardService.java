6f6ac5923f269fcfae7ea841253c070a31338741
AA\CRAMSLAN <ramsland.christian@epa.gov>
2023-04-18 12:16:33 -0400

    batch upload

--- src/main/java/gov/epa/databases/dev_qsar/qsar_models/service/PredictionDashboardService.java
+++ src/main/java/gov/epa/databases/dev_qsar/qsar_models/service/PredictionDashboardService.java
@@ -1,5 +1,7 @@
 package gov.epa.databases.dev_qsar.qsar_models.service;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.exception.ConstraintViolationException;
 
@@ -10,6 +12,10 @@ public interface PredictionDashboardService {
 	public PredictionDashboard create(PredictionDashboard predictionDashboard) throws ConstraintViolationException;
 	
 	public PredictionDashboard create(PredictionDashboard predictionDashboard, Session session) throws ConstraintViolationException;
+	
+	public List<PredictionDashboard> createBatch(List<PredictionDashboard> predictionDashboard) throws ConstraintViolationException;
+	
+	public List<PredictionDashboard> createBatch(List<PredictionDashboard> predictionDashboard, Session session) throws ConstraintViolationException;
 
 }
 
--- src/main/java/gov/epa/databases/dev_qsar/qsar_models/service/PredictionDashboardServiceImpl.java
+++ src/main/java/gov/epa/databases/dev_qsar/qsar_models/service/PredictionDashboardServiceImpl.java
@@ -1,5 +1,6 @@
 package gov.epa.databases.dev_qsar.qsar_models.service;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -13,6 +14,7 @@ import gov.epa.databases.dev_qsar.DevQsarValidator;
 import gov.epa.databases.dev_qsar.qsar_descriptors.QsarDescriptorsSession;
 import gov.epa.databases.dev_qsar.qsar_descriptors.entity.Compound;
 import gov.epa.databases.dev_qsar.qsar_models.QsarModelsSession;
+import gov.epa.databases.dev_qsar.qsar_models.entity.Bob;
 import gov.epa.databases.dev_qsar.qsar_models.entity.Prediction;
 import gov.epa.databases.dev_qsar.qsar_models.entity.PredictionDashboard;
 
@@ -32,8 +34,8 @@ public class PredictionDashboardServiceImpl implements PredictionDashboardServic
 
 
 	@Override
-	public PredictionDashboard create(PredictionDashboard prediction, Session session) throws ConstraintViolationException {
-		Set<ConstraintViolation<PredictionDashboard>> violations = validator.validate(prediction);
+	public PredictionDashboard create(PredictionDashboard predictionDashboard, Session session) throws ConstraintViolationException {
+		Set<ConstraintViolation<PredictionDashboard>> violations = validator.validate(predictionDashboard);
 
 		if (!violations.isEmpty()) {
 			throw new ConstraintViolationException(violations);
@@ -42,16 +44,48 @@ public class PredictionDashboardServiceImpl implements PredictionDashboardServic
 		Transaction t = session.beginTransaction();
 		
 		try {
-			session.save(prediction);
+			session.save(predictionDashboard);
 			session.flush();
-			session.refresh(prediction);
+			session.refresh(predictionDashboard);
 			t.commit();
 		} catch (org.hibernate.exception.ConstraintViolationException e) {
 			t.rollback();
 			throw new ConstraintViolationException(e.getMessage() + ": " + e.getSQLException().getMessage(), null);
 		}
 		
-		return prediction;
+		return predictionDashboard;
+	}
+
+
+	@Override
+	public List<PredictionDashboard> createBatch(List<PredictionDashboard> predictionDashboard)
+			throws org.hibernate.exception.ConstraintViolationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<PredictionDashboard> createBatch(List<PredictionDashboard> predictionDashboards, Session session)
+			throws org.hibernate.exception.ConstraintViolationException {
+		Transaction tx = session.beginTransaction();
+		try {
+		for (int i = 0; i < predictionDashboards.size(); i++) {
+			PredictionDashboard predictionDashboard = predictionDashboards.get(i);
+			session.save(predictionDashboard);
+		    if ( i % 1000 == 0 ) { //20, same as the JDBC batch size
+		        //flush a batch of inserts and release memory:
+		        session.flush();
+		        session.clear();
+		    }
+		}
+		} catch (org.hibernate.exception.ConstraintViolationException e) {
+			tx.rollback();
+		}
+		
+		tx.commit();
+		session.close();
+		return predictionDashboards;
 	}
 
 }
