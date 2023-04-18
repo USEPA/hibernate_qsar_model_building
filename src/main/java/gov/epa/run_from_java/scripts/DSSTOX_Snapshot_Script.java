82077087d936a9aad59e423b3804bd1eacf378e7
AA\TMARTI02 <martin.todd@epa.gov>
2023-04-18 10:52:54 -0400

    Update to DSSTOX_Snapshot script

--- src/main/java/gov/epa/run_from_java/scripts/DSSTOX_Snapshot_Script.java
+++ src/main/java/gov/epa/run_from_java/scripts/DSSTOX_Snapshot_Script.java
@@ -8,21 +8,29 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.List;
+
+import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.AtomContainerSet;
+import org.openscience.cdk.interfaces.IAtomContainer;
 
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
-
+import gov.epa.databases.dev_qsar.qsar_models.entity.Prediction;
 import gov.epa.databases.dsstox.entity.DsstoxCompound;
 import gov.epa.databases.dsstox.entity.GenericSubstance;
 import gov.epa.databases.dsstox.entity.GenericSubstanceCompound;
 import gov.epa.run_from_java.scripts.GetExpPropInfo.Utilities;
+import gov.epa.run_from_java.scripts.PredictionDashboard.RunDashboardPredictions;
 
 /**
 * @author TMARTI02
@@ -287,10 +295,184 @@ public class DSSTOX_Snapshot_Script {
 	
 	public static void main(String[] args) {
 		DSSTOX_Snapshot_Script d=new DSSTOX_Snapshot_Script();
-		checkmsReady();
+//		checkmsReady();
 //		d.getallcompounds();
 //		d.writeSDFs();
+		d.loadNames();
+		
 
+	}
+
+
+	class dsstox_name {
+		String dtxsid;
+		String dtxcid;
+		String IUPAC_Name;
+		String INDEX_Name;
+		String smiles;
+		String filename;
+		String created_by;
+		String updated_by;
+		
+		String iupac_name_0423;
+		String index_name_0423;
+		
+		//Constructor for create
+		public dsstox_name(String dtxsid,String dtxcid,String iupac_name_0423, String index_name_0423,String smiles,String filename, String created_by) {
+
+			this.dtxsid=dtxsid;
+			this.dtxcid=dtxcid;
+			this.smiles=smiles;
+			
+			this.index_name_0423=index_name_0423;
+			this.iupac_name_0423=iupac_name_0423;
+			
+			this.filename=filename;
+			this.created_by=created_by;
+		}
+		
+
+		//Constructor for update
+		public dsstox_name(String dtxcid,String iupac_name_0423, String index_name_0423,String smiles,String filename, String updated_by) {
+
+			this.dtxcid=dtxcid;
+			this.index_name_0423=iupac_name_0423;
+			this.iupac_name_0423=index_name_0423;
+			
+			this.smiles=smiles;
+			this.filename=filename;
+			this.updated_by=updated_by;
+		}
+
+		
+		
+		
+		
+		public void createSQL (List<dsstox_name> names) {
+
+			Connection conn=SqlUtilities.getConnectionPostgres();
+			
+			String [] fieldNames= {"dtxsid","dtxcid","IUPAC Name","INDEX Name","smiles","filename","created_by","created_at"};
+			int batchSize=1000;
+			
+			String sql="INSERT INTO qsar_models.predictions (";
+			
+			for (int i=0;i<fieldNames.length;i++) {
+				sql+=fieldNames[i];
+				if (i<fieldNames.length-1)sql+=",";
+				else sql+=") VALUES (";
+			}
+			
+			for (int i=0;i<fieldNames.length-1;i++) {
+				sql+="?";
+				if (i<fieldNames.length-1)sql+=",";			 		
+			}
+			sql+="current_timestamp)";	
+//			System.out.println(sql);
+			
+			try {
+				conn.setAutoCommit(false);
+				PreparedStatement prep = conn.prepareStatement(sql);
+				prep.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
+				long t1=System.currentTimeMillis();
+
+				for (int counter = 0; counter < names.size(); counter++) {
+					dsstox_name name=names.get(counter);
+					prep.setString(1, name.dtxsid);
+					prep.setString(2, name.dtxcid);
+					prep.setString(3, name.IUPAC_Name);
+					prep.setString(4, name.INDEX_Name);
+					prep.setString(5, name.smiles);
+					prep.setString(6, name.filename);
+					prep.setString(7, name.created_by);
+					prep.addBatch();
+					
+					if (counter % batchSize == 0 && counter!=0) {
+						// System.out.println(counter);
+						prep.executeBatch();
+					}
+				}
+
+				int[] count = prep.executeBatch();// do what's left
+				long t2=System.currentTimeMillis();
+				System.out.println("time to post "+names.size()+" names using batchsize=" +batchSize+":\t"+(t2-t1)/1000.0+" seconds");
+				conn.commit();
+//				conn.setAutoCommit(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}			
+		
+		void update (String lanId,String DTXCID,String IUPAC_Name,String INDEX_Name) {
+			String sql="UPDATE dsstox_names\r\n"
+					+ "SET iupac_name_0423 = '"+IUPAC_Name+"', "
+					+ "index_name_0423 = '"+INDEX_Name+"',"
+					+ "updated_by = '"+lanId+"'\r\n"
+					+ "WHERE dtxcid = '"+DTXCID+"123456';";
+			
+//			System.out.println(sql);
+			SqlUtilities.runSQLUpdate(conn, sql);
+		}
+	
+	}
+
+
+	private void loadNames() {
+		String lanId="tmarti02";
+		String folder="data\\dsstox\\names";
+		
+		File [] files=new File(folder).listFiles();
+		
+		File file=files[0];
+		
+		AtomContainerSet acs=RunDashboardPredictions.readSDFV3000(file.getAbsolutePath());
+		
+		boolean skipMissingSID=false;
+		int maxCount=99999999;
+		
+		AtomContainerSet acs2 = RunDashboardPredictions.filterAtomContainerSet(acs, skipMissingSID, maxCount);
+
+		Iterator<IAtomContainer> iterator= acs2.atomContainers().iterator();
+
+		
+		System.out.println(acs2.getAtomContainerCount());
+
+		int count=0;
+		
+		Connection conn=SqlUtilities.getConnectionDSSTOX();
+		
+		List<dsstox_name>names_update=new ArrayList<>();
+		List<dsstox_name>names_create=new ArrayList<>();
+		
+		while (iterator.hasNext()) {
+			count++;
+			
+			AtomContainer ac=(AtomContainer) iterator.next();
+			String smiles=ac.getProperty("smiles");
+			String DTXCID=ac.getProperty("DTXCID");
+			String DTXSID=ac.getProperty("DTXSID");
+			String IUPAC_Name=ac.getProperty("IUPAC_Name");
+			String INDEX_Name=ac.getProperty("INDEX_Name");
+			
+			
+			String sql1="select dtxcid from dsstox_names dn where dn.dtxcid ='"+DTXCID+"'";
+			
+			long t1=System.currentTimeMillis();
+			String dtxcid=SqlUtilities.runSQL(conn, sql1);
+			long t2=System.currentTimeMillis();
+			
+			if (dtxcid==null) {
+				names_create.add(new dsstox_name(DTXSID,DTXCID,IUPAC_Name, INDEX_Name,smiles,file.getName(), lanId));
+			} else {
+				names_update.add(new dsstox_name(DTXCID,IUPAC_Name, INDEX_Name,smiles,file.getName(), lanId));
+			}
+			
+			System.out.println(count+"\t"+DTXCID+"\t"+IUPAC_Name+"\t"+INDEX_Name+"\t"+(t2-t1));
+			
+		}
+
+		System.out.println(names_create.size()+"\t"+names_update.size());
+		
 	}
 
 }
